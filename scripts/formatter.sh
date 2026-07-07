836f7cac026a066e4f514cf48d26cc22a06b913c
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-07-07 07:47:08 +0200

    chore: more robust formatter script

--- scripts/formatter.sh
+++ scripts/formatter.sh
@@ -14,4 +14,4 @@ fi
 find \
     src \
     \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" -o -iname "*.c" \) \
-  -print0 | xargs -0 clang-format-14 "${MODE[@]}"
+  -print0 | xargs -0 -r -t clang-format-14 "${MODE[@]}"
